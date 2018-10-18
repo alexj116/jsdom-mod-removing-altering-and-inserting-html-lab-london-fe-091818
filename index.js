2b9ca3e8907f0d2dc6e19ef7f1ce730e32f42256
Alexandra Johnson <alexandra@queryclick.com>
2018-10-18 15:40:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 /* Enter the code to remove the main node element under this comment */
+const main=document.body.queryselector("main")
 
 /* Create your new element here and assign it to newHeader */
 const newHeader = null;
+
+var element = document.createElement('h1');
+document.newHeader.appendChild(element);
+HTMLElementObject.id = victory;
+h1.innerHTML= 'Alex is the champion';
